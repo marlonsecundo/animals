c04ce7fb6d07b4237a123dd77811468b13a18247
Davi Cesar de Araújo Bezerra <kayton@A307-17L.imd.local>
2019-06-18 12:05:44 -0300

    gets nas classes reotil e ave

--- ave.cpp
+++ ave.cpp
@@ -23,6 +23,21 @@ double Ave::get_tamanho_do_bico_cm()
 {
   return m_tamanho_do_bico_cm;
 }
+vector<string> Ave::get_dados()
+{
+  vector<string> dados;
+  //id;classe;nome;sexo;tamanho;dieta;batismo;tamanho_das_asas;tamanho_do_bico_cm
+  dados.push_back(to_string(get_id()));
+  dados.push_back(get_classe());
+  dados.push_back(get_nome_cientifico());
+  dados.push_back(to_string(get_sexo()));
+  dados.push_back(to_string(get_tamanho()));
+  dados.push_back(get_dieta());
+  dados.push_back(get_nome_batismo());
+  dados.push(to_string(get_envergadura_das_asas()));
+  dados.push(to_string(get_tamanho_do_bico_cm()));
+  return dados;
+}
 
 Ave::~Ave()
 {
--- reptil.h
+++ reptil.h
@@ -4,6 +4,7 @@
 
 #include <string>
 #include "animal.h"
+#include <vector>
 
 using std::cin;
 using std::cout;
@@ -17,14 +18,15 @@ protected:
   string m_tipo_venenoso;
 
 public:
-  Reptil();
+  Reptil(unsigned int novo_id, string novo_nome_batismo);
   void set_venenoso(bool venenoso);
   void set_tipo_venenoso(string tipo_veneno);
-
+  
+  vector<string> get_dados();  // id;classe;nome;sexo;tamanho;dieta;batismo;
+  
   bool get_venenoso();
   string get_tipo_venenoso();
 
-  Reptil(unsigned int novo_id, string novo_nome_batismo);
   ~Reptil();
 };
 
