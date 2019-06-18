66ea78cac6f1c1ac26bafbee59c00b715c8b830e
João Paulo Carneiro <joao.paulo_silva@icloud.com>
2019-06-17 21:26:52 -0300

    Adicionado o getDados() a anfibio e mamifero

--- mamifero.h
+++ mamifero.h
@@ -3,12 +3,14 @@
 #define MAMIFERO_H
 
 #include <string>
+#include <vector>
 #include "animal.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 class Mamifero : public Animal
 {
@@ -22,7 +24,8 @@ public:
   void set_cor_pelo(string cor_pelo);
 
   string get_cor_pelo();
-
+  vector<string> getDados();
+  
   ~Mamifero();
 };
 
