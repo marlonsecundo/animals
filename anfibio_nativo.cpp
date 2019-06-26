1481e8ee3d448060dd9458a2b2361da8abee7ed8
Marlon Secundo <marlon_secundo@outlook.com>
2019-06-25 22:09:08 -0300

    Anfibio Atualizado

--- anfibio_nativo.cpp
+++ anfibio_nativo.cpp
@@ -2,13 +2,4 @@
 
 AnfibioNativo::AnfibioNativo(map<string, string> dados) : Anfibio(dados), Animal_Nativo(dados["autorizacao_ibama"], dados["uf_origem"], dados["autorizacao"])
 {
-  set_classe(dados["classe"]);
-  set_nome_cientifico(dados["nome"]);
-  set_sexo(dados["sexo"][0]);
-  set_tamanho(stod(dados["tamanho"]));
-  set_dieta(dados["dieta"]);
-  set_total_mudas(stod(dados["total_mudas"]));
-  set_data_string(dados["ultima_muda"]);
-  set_veterinario(dados["veterinario"]);
-  set_tratador(dados["tratador"]);
 }
